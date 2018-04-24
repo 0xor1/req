cc280b4ae11419473bcf9b03dbb4ce46e1c704d1
daniel robinson <daniel.robinson.open@gmail.com>
2018-04-24 12:16:55 +0100

    init

---
+++ main.go
@@ -0,0 +1,42 @@
+package main
+
+import (
+	"os"
+	"fmt"
+	"net/http"
+	"io/ioutil"
+)
+
+func main() {
+	if len(os.Args) < 3 {
+		fmt.Println("req usages:\nreq METHOD URL [-c COOKIE_NAME COOKIE_VALUE] [-h HEADER_NAME HEADER VALUE]\nreq GET http://example.com -c myCookie 123abc -h Authorization myUsr&Pwd")
+		os.Exit(0)
+	}
+	req, e := http.NewRequest(os.Args[1], os.Args[2], nil)
+	for i := range os.Args {
+		if os.Args[i] == "-h" {
+			fmt.Println(os.Args[i+1], os.Args[i+2])
+			req.Header.Set(os.Args[i+1], os.Args[i+2])
+		} else if os.Args[i] == "-c" {
+			req.AddCookie(&http.Cookie{Name: os.Args[i+1], Value: os.Args[i+2]})
+		}
+	}
+	logAndExitIf(e)
+	resp, e := http.DefaultClient.Do(req)
+	logAndExitIf(e)
+	if resp != nil && resp.Body != nil {
+		defer resp.Body.Close()
+	}
+	body, e := ioutil.ReadAll(resp.Body)
+	logAndExitIf(e)
+	fmt.Println("Status: ", resp.StatusCode)
+	fmt.Println("Headers: ", resp.Header)
+	fmt.Println("Body: ", string(body))
+}
+
+func logAndExitIf(e error) {
+	if e != nil {
+		fmt.Println(e)
+		os.Exit(1)
+	}
+}
---
+++ readme.md
@@ -0,0 +1,12 @@
+req
+===
+
+A Simple cmdln tool to make http requests.
+
+```
+go install github.com/0xor1/req
+#general
+req METHOD URL [-c COOKIE_NAME COOKIE_VALUE] [-h HEADER_NAME HEADER VALUE]
+#example
+req GET http://example.com -c myCookie 123abc -h Authorization myUsr&Pwd
+```
