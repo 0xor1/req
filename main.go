a516964ce4cb77be1ad2820997bcb69fab9c864d
daniel robinson <daniel.robinson.open@gmail.com>
2018-06-14 11:16:04 +0100

    tweak

--- main.go
+++ main.go
@@ -9,15 +9,24 @@ import (
 
 func main() {
 	if len(os.Args) < 3 {
-		fmt.Println("general usage:\nreq METHOD URL [-c COOKIE_NAME COOKIE_VALUE] [-h HEADER_NAME HEADER_VALUE]\nexample usage:\nreq GET http://example.com -c myCookie 123abc -h Authorization myUsr&Pwd")
+		fmt.Println("general usage:\nreq METHOD URL [-ba USERNAME PASSWORD] [-ta BEARER_TOKEN] [-c COOKIE_NAME COOKIE_VALUE] [-h HEADER_NAME HEADER_VALUE]\nexample usage:\nreq GET http://example.com -ba testuser testpwd -c myCookie 123abc -h headerName headerValue")
 		os.Exit(0)
 	}
 	req, e := http.NewRequest(os.Args[1], os.Args[2], nil)
+	printBody := true
+	printHeader := true
 	for i := range os.Args {
-		if os.Args[i] == "-h" {
-			fmt.Println(os.Args[i+1], os.Args[i+2])
-			req.Header.Set(os.Args[i+1], os.Args[i+2])
-		} else if os.Args[i] == "-c" {
+		if os.Args[i] == "-nb" {
+			printBody = false
+		} else if os.Args[i] == "-nh" {
+			printHeader = false
+		} else if os.Args[i] == "-ta" { // bearer token auth
+			req.Header.Add("Authorization", "Bearer " + os.Args[i+1])
+		} else if os.Args[i] == "-ba" { // basic auth
+			req.SetBasicAuth(os.Args[i+1], os.Args[i+2])
+		} else if os.Args[i] == "-h" { // add header
+			req.Header.Add(os.Args[i+1], os.Args[i+2])
+		} else if os.Args[i] == "-c" { // add cookie
 			req.AddCookie(&http.Cookie{Name: os.Args[i+1], Value: os.Args[i+2]})
 		}
 	}
@@ -30,8 +39,12 @@ func main() {
 	body, e := ioutil.ReadAll(resp.Body)
 	logAndExitIf(e)
 	fmt.Println("Status: ", resp.StatusCode)
-	fmt.Println("Headers: ", resp.Header)
-	fmt.Println("Body: ", string(body))
+	if printHeader {
+		fmt.Println("Headers: ", resp.Header)
+	}
+	if printBody {
+		fmt.Println("Body: ", string(body))
+	}
 }
 
 func logAndExitIf(e error) {
